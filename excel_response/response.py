34aa2d7f359ddbafd345006594b2acf7eb2c766a
Panagiotis H.M. Issaris <takis@issaris.com>
2021-02-27 00:19:18 +0100

    The guess_types parameter was removed in openpyxl 3.0.3

--- excel_response/response.py
+++ excel_response/response.py
@@ -21,14 +21,13 @@ class ExcelResponse(HttpResponse):
     """
 
     def __init__(self, data, output_filename='excel_data', worksheet_name=None, force_csv=False, header_font=None,
-                 data_font=None, guess_types=True, *args, **kwargs):
+                 data_font=None, *args, **kwargs):
         # We do not initialize this with streaming_content, as that gets generated when needed
         self.output_filename = output_filename
         self.worksheet_name = worksheet_name or 'Sheet 1'
         self.header_font = header_font
         self.data_font = data_font
         self.force_csv = force_csv
-        self.guess_types = guess_types
         super().__init__(data, *args, **kwargs)
 
     @property
@@ -72,7 +71,6 @@ class ExcelResponse(HttpResponse):
             write_header = append
         else:
             workbook = Workbook(write_only=True)
-            workbook.guess_types = self.guess_types
             worksheet = workbook.create_sheet(title=self.worksheet_name)
 
             # Define custom functions for appending so that we can handle any formatting
